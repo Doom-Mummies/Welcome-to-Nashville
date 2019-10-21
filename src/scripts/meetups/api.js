143face263c6af7acce10352c558fcae61bdaac4
Lauren Riddle <lari7132@gmail.com>
2019-10-21 15:10:43 -0500

    added fetch call to get information from API

--- src/scripts/meetups/api.js
+++ src/scripts/meetups/api.js
@@ -1,3 +1,6 @@
 
+    // .then(results => console.log(results));
 
 
+    const searchMeetups= userInput => fetch("https://raw.githubusercontent.com/nss-cohort-36/temp-eb-api/master/search-response.json")
+    .then(result => result.json())
