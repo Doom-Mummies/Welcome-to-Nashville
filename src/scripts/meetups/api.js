5400306dad6c8405b76fc8114dd07191bf5b494f
Lauren Riddle <lari7132@gmail.com>
2019-10-21 15:55:30 -0500

    working on API

--- src/scripts/meetups/api.js
+++ src/scripts/meetups/api.js
@@ -4,44 +4,44 @@
 
 
 
-//     const handleMeetupSearch = event => {
-//         // console.log(event)
-//         const inputField = document.querySelector("#results-container")
-//         console.log("user input", inputField.value)
+    const handleMeetupSearch = event => {
+        // console.log(event)
+        const inputField = document.querySelector("#results-container")
+        console.log("user input", inputField.value)
       
-//         searchMeetups(inputField.value)
-//         .then(results => {
-//           // console.log(response.recipes)
-//           displayMeetupHtml(results.events)
-//           inputField.value = ""
-//         })
-//       }
+        searchMeetups(inputField.value)
+        .then(results => {
+          // console.log(response.events)
+          displayMeetupHtml(results.events)
+          inputField.value = ""
+        })
+      }
       
-//       const attachEventListenerToSearchButton = () => {
-//         const searchButton = document.getElementById("meetups-search-button")
-//         searchButton.addEventListener("click", handleSearch)
-//       }
+      const attachEventListenerToSearchButton = () => {
+        const searchButton = document.getElementById("meetups-search-button")
+        searchButton.addEventListener("click", handleSearch)
+      }
 
 
 
-//     const buildMeetupHtml = meetup => `
-// <section>
-//   <h4>${recipe.title}</h4>
-//   <img src="${recipe.image_url}" alt="Recipe Image">
-//   <p>
-//       <a href="${recipe.source_url}">Click here to see the full recipe</a>
-//   </p>
-// </section>
-// `
+    const buildMeetupHtml = meetup => `
+<section>
+  <h4>${recipe.title}</h4>
+  <img src="${recipe.image_url}" alt="Recipe Image">
+  <p>
+      <a href="${recipe.source_url}">Click here to see the full recipe</a>
+  </p>
+</section>
+`
 
 
-// const displayRecipeHtml = allRecipes => {
-//   let recipeResultsHtml = ""
-//   allRecipes.forEach(recipe => {
-//     let recipeHtml = buildRecipeHtml(recipe)
-//     recipeResultsHtml += recipeHtml
-//   });
+const displayRecipeHtml = allRecipes => {
+  let recipeResultsHtml = ""
+  allRecipes.forEach(recipe => {
+    let recipeHtml = buildRecipeHtml(recipe)
+    recipeResultsHtml += recipeHtml
+  });
 
-//   const searchResultsSection = document.querySelector(".search-results")
-//   searchResultsSection.innerHTML = recipeResultsHtml
-// }
+  const searchResultsSection = document.querySelector(".search-results")
+  searchResultsSection.innerHTML = recipeResultsHtml
+}
