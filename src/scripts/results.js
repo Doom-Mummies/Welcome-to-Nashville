088d76ad101bd44f506593362ca5deadcb04fca2
Michelle Johnson <bob1234girl@gmail.com>
2019-10-22 14:13:40 -0500

    added colon after restaurant name in results

--- src/scripts/results.js
+++ src/scripts/results.js
@@ -20,7 +20,7 @@ const displayMeetupHtml = allMeetups => {
 // html to be put into .search-results in searchForm.js
 const buildRestaurantHtml = restaurant => `
 <article>
-  <h4>${restaurant.restaurant.name}</h4>
+  <h4>${restaurant.restaurant.name}:</h4>
   <p>
       ${restaurant.restaurant.location.address}
   </p>
