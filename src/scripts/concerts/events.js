7a4b18d86825ba25ee8be53f1666ed563e8c9eff
rhc822 <rhc822@gmail.com>
2019-10-22 15:06:55 -0500

    Went back and added comments to my work

--- src/scripts/concerts/events.js
+++ src/scripts/concerts/events.js
@@ -1,7 +1,13 @@
+/* Put this whole section in a function and referenced that in the main.js file at the end of index.html to ensure everything has been initialized. */ 
+
+
 function displayGenreSearch() {
-    const concertsSearchButton = document.getElementById("concerts-search-button")
+    // Reference the search button for the concerts section...
+    const concertsSearchButton = document.getElementById("concerts-search-button") 
     
+    // ...and add an event listener to it that runs a function...
     concertsSearchButton.addEventListener("click", () => {
+        // ...which takes references the search box and (currently) simply console logs the value that is entered into it
         const concertsGenreSearchBox = document.querySelector("#concertsGenreSearch")
         console.log(concertsGenreSearchBox.value)
 })
--- src/scripts/key.js
+++ src/scripts/key.js
@@ -1,3 +1,4 @@
+// The Ticketmaster API key for the concert
 const concertsTicketMasterKey = "LJiodGFvXhaw9a8blqRbrMKTj4aiYv2Z";
 const zomatoApiKey = "992c2af9324bd1d186adf52511b0da0a"
 const eventbriteAPIKey = "YOUR KEY GOES HERE"
