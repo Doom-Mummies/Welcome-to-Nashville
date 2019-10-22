e676db92191d53b95bab8f4292a5afd855188fec
Michelle Johnson <bob1234girl@gmail.com>
2019-10-22 09:29:21 -0500

    trying to get api to work

--- src/scripts/restaurants/api.js
+++ src/scripts/restaurants/api.js
@@ -1,4 +1,11 @@
 const apiBaseUrl = "https://developers.zomato.com/api"
 
-const searchResaurants = searchInput => fetch(`${apiBaseUrl}/search?apikey=${zomatoApiKey}&q=${searchInput}`)
-.then(response => response.json())
+const searchResaurants = searchInput => 
+    fetch(`${apiBaseUrl}/v2.1/search?entity_id=1138&entity_type=city&q=${searchInput}&sort=rating}`)
+        "user-key: ${zomatoApiKey}",
+        "method: GET",
+        "headers:" 
+            "Accept: application/json"
+        
+    .then(response => response.json())
+
