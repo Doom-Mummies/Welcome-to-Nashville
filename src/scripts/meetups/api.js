cd58ad9bfdbcefc86fddbd23bd9da72b6cbcc734
Lauren Riddle <lari7132@gmail.com>
2019-10-22 11:06:16 -0500

    built meetup html function to create the results element html when the search button is clicked

--- src/scripts/meetups/api.js
+++ src/scripts/meetups/api.js
@@ -1,43 +1,25 @@
 const searchMeetups = userInput => fetch("https://raw.githubusercontent.com/nss-cohort-36/temp-eb-api/master/search-response.json")
-.then(result => result.json())
+  .then(result => result.json())
 // .then(results => console.log(results));
 
-const buildMeetupHtml = events => `
-<section class="meetupEvent">
-<p>${events.venue.name}${events.venue.address}</p>
-</section>`
-
-const handleMeetupSearch = event => {
-    // console.log(event)
-    const inputField = document.getElementById("input-3")
-    console.log("user input", inputField.value)
-  
-    searchMeetups(inputField.value)
-    .then(results => {
-      // console.log(response.events)
-      displayMeetupHtml(results.events)
-      inputField.value = ""
-    })
-  }
 const attachEventListenerToSearchButton = () => {
   const searchButton = document.querySelector("#meetups-search-button")
   searchButton.addEventListener("click", handleMeetupSearch)
 }
 
+const handleMeetupSearch = event => {
+  // console.log(event)
+  const inputField = document.getElementById("input-3")
+  console.log("user input", inputField.value)
+}
 
-// const displayMeetupHtml = allMeetups => {
-//   let MeetupSearchResultsHtml = ""
-//   allMeetups.forEach(event => {
-//     let MeetupHtml = buildMeetupHTML(event)
-//     MeetupSearchResultsHtml += buildMeetupHtml
-//   });
 
-//   const searchResultsSection = document.querySelector("#results-container")
-//   searchResultsSection.innerHTML = MeetupSearchResultsHtml
-//   console.log(MeetupSearchResultsHtml)
-// }
+ const buildMeetupHtml = events => `
+ <section class="meetupEvent">
+ ${events[0].name.html}${events[0].description.html}
+ </section>`
 
-      
+
 
 
 
