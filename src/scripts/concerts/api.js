ef72de18f9594dfe6fb432e6c6770a59526385ed
rhc822 <rhc822@gmail.com>
2019-10-23 14:53:54 -0500

    continuing to work

--- src/scripts/concerts/api.js
+++ src/scripts/concerts/api.js
@@ -5,6 +5,6 @@ const ticketMasterBaseUrl = "https://app.ticketmaster.com/discovery/v2/events.js
 const searchTicketMaster = (searchInput) => fetch(`${ticketMasterBaseUrl}?apikey=${concertsTicketMasterKey}&stateCode=TN&classificationName=music&dmaID=343&classificationName=${searchInput}`)
     .then(rawApiData => rawApiData.json()) //Takes the results, parses it, and then...
     .then(searchResults => {
-        console.log((searchResults._embedded.events))
+        console.log(searchResults._embedded.events)
         // displaySearchResults(searchResults._embedded.events)
     }) //...Runs the following function, passing only the "events" information in. Function definition is on "results.js"
